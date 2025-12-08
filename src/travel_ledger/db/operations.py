08badbed46739f18bf8b1a42019661beb6e3e8a8
ian-khan <66009288+ian-khan@users.noreply.github.com>
2025-12-08 16:33:01 +0800

    Defined an execute() helper function in operations.py for safer, cleaner implementation of task-specific functions.

--- src/travel_ledger/db/operations.py
+++ src/travel_ledger/db/operations.py
@@ -1,14 +1,37 @@
 import sqlite3
 from travel_ledger.db.statements import (build_create_table_stmt, build_insert_stmt_params,
                                          build_update_stmt_params, build_select_one_stmt)
+from contextlib import closing
+from typing import Any, Optional, Sequence
 
+def execute(db_path: str,
+            stmt: str,
+            params: Sequence[Any]=(),
+            fetch: Optional[str]=None):
+    """
+    Open connection, execute a statement with optional parameters, and return the fetched result.
+    :param db_path: path to database
+    :param stmt: SQL statement to execute
+    :param params: parameters to pass to statement
+    :param fetch: None, 'one', or 'all'
+    :return: fetched result
+    """
+    if fetch not in (None, 'one', 'all'):
+        raise ValueError("fetch must be None, 'one', or 'all'")
+    # Automatically close connection
+    with closing(sqlite3.connect(db_path)) as con:
+        # Automatically commit or roll back transaction
+        with con:
+            cur = con.cursor()
+            cur.execute(stmt, params)
+            fetched = None
+            if fetch in ('one', 'all'):
+                fetched = getattr(cur, f'fetch{fetch}')()
+    return fetched
 
-def create_table(db_name: str):
-    con = sqlite3.connect(db_name)
-    cur = con.cursor()
-    cur.execute(build_create_table_stmt())
-    con.commit()
-    con.close()
+def create_table(db_path: str):
+    stmt = build_create_table_stmt()
+    execute(db_path, stmt)
 
 def insert_record(db_path: str, record: dict):
     con = sqlite3.connect(db_path)
